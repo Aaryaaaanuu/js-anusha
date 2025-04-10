b751e95dd9e80a3008818d355110c06689d68093
Anusha Pokharel <60640045+Aaryaaaanuu@users.noreply.github.com>
2025-04-10 11:09:08 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14453
+let accountEmail = "anusha@gmail.com"
+var accountPassword = "13456"
+accountCity="Pokhara"
+let accountState;
+//  accountId = 3 not allowed
+accountEmail ="anu@gm.com"
+accountPassword = "485757"
+accountCity="ktm"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
