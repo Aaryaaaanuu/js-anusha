1867cb857b4e9c3a1cf97926754c3c0ccca12b02
Anusha Pokharel <60640045+Aaryaaaanuu@users.noreply.github.com>
2025-04-10 11:07:27 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer wersion
+// alert(3+3) // we are using nodejs, not browser
+console.log (3+3)
+console.log ("Anusha")
+
+let name ="Anusha"
+let age = 18
+let isLoggedIn = false 
+let state;  // undefined
+
+console.log(typeof null); // object
+console.log (typeof undefined); // undefined 
