600367c8e405404b15f528e24211f949451cbcf7
Anusha Pokharel <60640045+Aaryaaaanuu@users.noreply.github.com>
2025-04-15 08:06:24 +0000

    changes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,30 +1,30 @@
-// let score = "33"
-// console.log(typeof score);
-// console.log(typeof(score)); // it is same way as above but its called the method way
-// //now lets do the type conversion
-// let valueInNumber = Number (score); // this converts "33ab" into a number using Number() function.
-// let str1 = "hello"
-// let str3 = " Anusha" // you need to give space here to have space in between
-// let str4 = str1 + str3
-// console.log(str4);
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score)); // it is same way as above but its called the method way
+//now lets do the type conversion
+let valueInNumber = Number (score); // this converts "33ab" into a number using Number() function.
+let str1 = "hello"
+let str3 = " Anusha" // you need to give space here to have space in between
+let str4 = str1 + str3
+console.log(str4);
 
 //operations
-// console.log( 3**3); // it becomes 3^3
-//console.log(3^3); // this gives 0 does not gives power in js
+console.log( 3**3); // it becomes 3^3
+console.log(3^3); // this gives 0 does not gives power in js
 
 //string addition
-// console.log("1" + 3); //op =13 as string is in the first
-// console.log(1 + "3"); // op=13 as it just concatenate the number with the srting
-// console.log("1" + 3 + 3); //op=133
-// console.log(1 + 3 + "3") //op= 43 as it adds the 1st two strings and concatenate the last string with the previous addition
+console.log("1" + 3); //op =13 as string is in the first
+console.log(1 + "3"); // op=13 as it just concatenate the number with the srting
+console.log("1" + 3 + 3); //op=133
+console.log(1 + 3 + "3") //op= 43 as it adds the 1st two strings and concatenate the last string with the previous addition
 
-// special or tricky conversion
-// unary plus operations: converts  its operand to a number
-// console.log(+true); //true=1
-// console.log(+false); //false obvi=0
-// console.log(+""); //empty bracket=0
-// console.log(+null); //null=0
-// console.log(+[]); // empty array =0
+//special or tricky conversion
+//unary plus operations: converts  its operand to a number
+console.log(+true); //true=1
+console.log(+false); //false obvi=0
+console.log(+""); //empty bracket=0
+console.log(+null); //null=0
+console.log(+[]); // empty array =0
 
 //perfix and postfix operators
 let a = 5;
--- .devcontainer/01_basics/04_comparision.js
+++ .devcontainer/01_basics/04_comparision.js
@@ -1,1 +1,12 @@
-console.log(3+4);
+console.log(undefined == 0);  // false :undefined is not loosely equal to 0 ,Only null == undefined is true,  So this is false
+console.log(undefined > 0);   // false :NaN > 0 → false
+console.log(undefined >= 0);  // false :NaN >= 0 → false
+ 
+console.log(null == 0); //false but null` is **only loosely equal to undefined**, not to `0`
+console.log(null > 0);  //false 0>0
+console.log(null >= 0); //true  0>=0
+//In comparisons like `>=` or `<`, `null` gets converted to a number (`0`), but in `==`, it doesn’t.
+
+// == vs === (=== checks more strictly as it also checks its data type)
+console.log("3"=== 3); //false
+console.log("4"== 4); //true
