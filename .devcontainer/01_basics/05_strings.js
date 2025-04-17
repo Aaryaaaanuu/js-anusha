feac6f37e38ddef630b46e6390b6df1cdf5188e1
Anusha Pokharel <60640045+Aaryaaaanuu@users.noreply.github.com>
2025-04-17 08:08:41 +0000

    changes

--- .devcontainer/01_basics/05_strings.js
+++ .devcontainer/01_basics/05_strings.js
@@ -1,9 +1,30 @@
 const name = "Anusha" // string literal stored in stack memory
 const repoCount = 3
 console.log(`Hello my name is ${name} and my repo number is ${repoCount}`) //(backticks `) for string interpolation. so ${name} and ${repoCount} insert the variable values directly into the string.
-const gameName = new String ('Anusha') // string object stored in heap memory
+const gameName = new String ('Anusha-Pokharel-com') // string object stored in heap memory
 console.log (gameName[3]); //op= 3 0=A 1=n 2=u 3=s..
 console.log (gameName.length); //op=6
 console.log (gameName.toUpperCase());
 console.log (gameName.charAt(4));
 console.log (gameName.indexOf("s"));
+
+const anu = "Anusha"
+console.log(anu); //   Anusha
+console.log(anu.trim()); //Anusha
+
+// substing mehtod
+ const aarru = anu.substring(3, 5); //op= nu
+ console.log(aarru); 
+//  const hello = anu.slice(-8, 4);
+//  console.log(hello);
+
+ //cleaner URLs
+ const url = "https://anusha.com/anusha%20pokharel" //the - (hyphen or dash) is used to replace %20, which represents a space in a URL.
+ console.log(url.replace("%20","-"));
+ const anuuu = "Anuzha"
+ console.log(anuuu.replace("z","s")); //op=Anusha
+
+ console.log(url.includes("anusha"));//true
+ console.log(url.includes("asp")); //false
+ console.log(gameName.split("-")); //op=[ 'Anusha', 'Pokharel', 'com' ]  converts strings to array
+ 
