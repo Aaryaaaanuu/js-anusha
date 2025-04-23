b95fb81c2a597c5050a6994b1e3c9fad5575ab18
Anusha Pokharel <60640045+Aaryaaaanuu@users.noreply.github.com>
2025-04-23 04:45:26 +0000

    change

---
+++ .devcontainer/01_basics/07_datesinJs.js
@@ -0,0 +1,14 @@
+// let myDate = new Date() //This creates a new Date object that holds the current date and time.
+// console.log(myDate.toString()); //Fri Apr 18 2025 13:42:55 GMT+0545 (Nepal Time) including the timezone
+// console.log(myDate.toDateString()); //This gives you just the date, no time info.
+// console.log(myDate.toLocaleString());//This formats the date and time based on your computer's local settings (like your region/language).
+// console.log(typeof myDate); //object
+//  let myTimeStamp = Date.now()
+//  console.log(Date.now()); // gives the time in milisecond.It’s a big number — representing the total milliseconds passed since 1970-01-01 00:00:00 UTC.
+ 
+newDate.toLocalString('default',{
+weekday: "long"
+})
+
+// array
+const myArr = [1, 3, 4, 5]
